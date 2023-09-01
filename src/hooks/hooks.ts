daa504e7ca6fc4ddd44b45d6a52b9cd8630950a9
nurulsilpia <nurul.silpia@jejakin.com>
2023-09-01 15:58:59 +0700

    add missing variabel

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -5,6 +5,7 @@ import { getEnv } from "../helper/env/env";
 import { invokeBrowser } from "../helper/browsers/browserManager";
 import { createLogger } from "winston";
 import { options } from "../util/logger";
+const fs = require("fs-extra");
 
 let browser: Browser;
 let context: BrowserContext;
@@ -17,7 +18,11 @@ BeforeAll(async () => {
 
 Before(async function ({pickle}) {
     const scenarioName = pickle.name+pickle.id;
-    context = await browser.newContext();
+    context = await browser.newContext({
+        recordVideo: {
+            dir: "test-results/videos",
+        }
+    });
     const page = await browser.newPage();
     pageFixture.page = page;
     pageFixture.logger = createLogger(options(scenarioName));
@@ -29,24 +34,27 @@ Before(async function ({pickle}) {
 }); */
 
 After(async function ({ pickle, result }) {
-    console.log(result?.status);
-
+    let videoPath: string;
+    let img: Buffer;
     if (result?.status == Status.FAILED) {
-        const img = await pageFixture.page.screenshot({ path: `./test-results/screenshots/${pickle.name}.png`, type: "png" })
-        await this.attach(img, "image/png");
-
-        //attach video
-        const video = await pageFixture.page.video();
-        const videoPath = `./test-results/videos/${pickle.name}.webm`;
-        await video.saveAs(videoPath);
-        await this.attach(videoPath, "video/webm");
+        img = await pageFixture.page.screenshot({ path: `./test-results/screenshots/${pickle.name}.png`, type: "png" })
+        videoPath = await pageFixture.page.video().path();
     }
-
     await pageFixture.page.close();
     await context.close();
+    if (result?.status == Status.FAILED) {
+        await this.attach(
+            img, "image/png"
+        );
+        await this.attach(
+            fs.readFileSync(videoPath),
+            'video/webm'
+        );
+    }
+
 });
 
 AfterAll(async () => {
     await browser.close();
-    pageFixture.logger.close();
+    // pageFixture.logger.close();
 })
